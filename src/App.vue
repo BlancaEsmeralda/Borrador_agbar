79642724c32e8a9d2378961c1cad190260dac728
Blanca Esmeralda <blanca.esmeralda@aiguesdebarcelona.cat>
2025-01-22 10:59:50 +0100

    header cambios componente
    pruebas del header.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <HeaderMain />
+    <router-link />
   </div>
 </template>
 <script>
--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -1,31 +1,29 @@
 <template>
-  <header class="main-header">
+  <div class="header-wrapper">
     <img
       loading="lazy"
       src="https://cdn.builder.io/api/v1/image/assets/TEMP/69bb028aa17a6a0bccca69ca0982fdd3b5bf18d4d10e6412766bbcdf02db6477?placeholderIfAbsent=true&apiKey=b652270817834812b1544511c6442c69"
       alt="Company Logo"
-      class="logo-image"
+      class="company-logo"
     />
-  </header>
+  </div>
 </template>
 
 <style scoped>
-.main-header {
+.header-wrapper {
   background-color: rgba(0, 47, 108, 1);
   display: flex;
   flex-direction: column;
-  align-items: start;
+  align-items: flex-start;
   justify-content: center;
   padding: 24px 75px;
 }
-
 @media (max-width: 991px) {
-  .main-header {
+  .header-wrapper {
     padding: 0 20px;
   }
 }
-
-.logo-image {
+.company-logo {
   aspect-ratio: 3.48;
   object-fit: contain;
   object-position: center;
